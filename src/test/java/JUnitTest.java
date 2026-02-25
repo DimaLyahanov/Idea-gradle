81ba0cde95c531524f68417003c395b9f2616274
DimaLyahanov <zerozero1488322@gmail.com>
2026-02-25 18:51:16 +0300

    фывфыв

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class JUnitTest {
+
+      @Test
+    void firstTest() {
+          Assertions.assertTrue(3>2);
+    }
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3>2);
+    }
+
+    @Test
+    void thirdTest() {
+        Assertions.assertTrue(3>2);
+    }
+
 }
